0e357c9d8f34c0f9ed98a8d174bc419a5282805f
Kevin Baker <kbaker88@users.noreply.github.com>
2017-05-22 22:19:08 -0700

    Sync

---
+++ DirectX_Playground/main.cpp
@@ -0,0 +1,99 @@
+#include <Windows.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+typedef HGLRC __stdcall wgl_create_context_attribs_arb(HDC hDC,
+	HGLRC hShareContext, const int *attribList);
+
+LRESULT CALLBACK WndProc(HWND WindowPtr, UINT msg, WPARAM wParam,
+	LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int CommandShow)
+{
+	WNDCLASSEX WindowsClassStructure;
+	WindowsClassStructure.cbSize = sizeof(WNDCLASSEX);
+	WindowsClassStructure.style = CS_OWNDC;
+	WindowsClassStructure.lpfnWndProc = WndProc;
+	WindowsClassStructure.cbClsExtra = 0;
+	WindowsClassStructure.cbWndExtra = 0;
+	WindowsClassStructure.hInstance = hInstance;
+	WindowsClassStructure.hIcon = LoadIcon(0, IDI_APPLICATION);
+	WindowsClassStructure.hCursor = LoadCursor(0, IDC_ARROW);
+	WindowsClassStructure.hbrBackground = (HBRUSH)(COLOR_WINDOW + 3);
+	WindowsClassStructure.lpszMenuName = 0;
+	WindowsClassStructure.lpszClassName =
+		(const char*)"VulkanPlayground";
+	WindowsClassStructure.hIconSm = LoadIcon(0, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&WindowsClassStructure))
+	{
+		MessageBox(0, "Window Registration Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+	}
+
+	HWND WindowPtr = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		(const char*)"VulkanPlayground",
+		"Vulkan Playground",
+		WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^ WS_MAXIMIZEBOX,
+		CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT,
+		0, 0, hInstance, 0);
+
+	if (!WindowPtr)
+	{
+		MessageBox(0, "Window Creation Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+	}
+
+	UpdateWindow(WindowPtr);
+	ShowWindow(WindowPtr, CommandShow);
+
+	//HDC WindowDeviceContext = GetDC(WindowPtr);
+
+	MSG Message = {};
+	while (Message.message != WM_QUIT)
+	{
+		if (PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&Message);
+			DispatchMessage(&Message);
+		}
+	}
+
+	return Message.message;
+}
+
+
+LRESULT CALLBACK WndProc(HWND WindowPtr, UINT Message,
+	WPARAM wParam, LPARAM lParam)
+{
+	switch (Message)
+	{
+	case WM_CREATE:
+		break;
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_SIZE:
+	{
+
+	} break;
+	case WM_CHAR:
+	{
+		if (wParam == VK_ESCAPE)
+		{
+			PostQuitMessage(0);
+		}
+	} break;
+
+	default:
+		return DefWindowProc(WindowPtr, Message, wParam, lParam);
+	}
+
+	return 0;
+}
